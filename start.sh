ef1088b8809b56a62f0857637ab071a1928c247a
root <root@dimas.kovas.mskhost.pro>
2020-12-13 20:00:07 +0000

    update

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ docker rm -f czero
 docker run \
     --name czero \
     -t \
+    --restart=always \
     --network host \
     czero:latest "hlds_linux -game czero -pidfile +map aa_dima2 +maxplayers 32 -autoupdate -nohltv sys_ticrate 10000"
